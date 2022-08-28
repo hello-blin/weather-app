de181ba5456c29cb422877f6c70d13b6cdb72687
Blin Kukaj <kblin164@gmail.com>
2022-08-28 20:32:47 +0200

    Inittial Push after I have made and initialized some features.

---
+++ app.js
@@ -0,0 +1,12 @@
+const request = require("request");
+
+const url =
+  "http://api.weatherstack.com/current?access_key=908f2e487a0a63fce389ab9c3a1fb41c&query=42.6629,21.1655";
+
+request({ url: url, json: true }, (error, response) => {
+  console.log(response.body.current);
+});
+
+
+
+

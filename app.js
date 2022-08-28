ca9b7402464a6e8fed176d8558d19c61c7b48195
Blin Kukaj <kblin164@gmail.com>
2022-08-28 21:12:45 +0200

    Added another request function. Also added some comments to explain what I did

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 const request = require("request");
 
+/*This is the base URL that we will fetch data from.
+ We use access key to link our secret key that the API owner generated for me and we use query string 
+ which is an option to choose city by langitude and latitude at the end
+*/
+
 const url =
   "http://api.weatherstack.com/current?access_key=908f2e487a0a63fce389ab9c3a1fb41c&query=42.6629,21.1655";
 
+//Samples for requesting a wholestack data from weatherstack API.
 request({ url: url, json: true }, (error, response) => {
-  console.log(response.body.location.name);
+  console.log(response.body);
 });
 
+//Samples for requesting detailed data from weatherstack API.
+//We go now by getting only the latest observation time that our data was captured
+//The parameter: 'json: true' will parse our providen data to JSON.
+request({ url: url, json: true }, (error, response) => {
+  console.log(response.body.current.observation_time);
+});
 
-
-
+request({url:url})

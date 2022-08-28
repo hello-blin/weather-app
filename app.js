177a69bde57d9c768200da912399c6c578d36f25
Blin Kukaj <kblin164@gmail.com>
2022-08-28 20:55:22 +0200

    Initial commit with a request only made.

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const url =
   "http://api.weatherstack.com/current?access_key=908f2e487a0a63fce389ab9c3a1fb41c&query=42.6629,21.1655";
 
 request({ url: url, json: true }, (error, response) => {
-  console.log(response.body.current);
+  console.log(response.body.location.name);
 });
 
 
